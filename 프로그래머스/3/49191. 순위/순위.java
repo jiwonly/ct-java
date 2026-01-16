3bf23df017bed56a086f2f50e782a27b7df63469
Jiwon <124144606+jiwonly@users.noreply.github.com>
2026-01-16 12:41:33 +0900

    [level 3] Title: 순위, Time: 11.94 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] graph = new boolean[n+1][n+1];
+        int answer=0;
+        
+        for(int[] result : results){
+            graph[result[0]][result[1]]=true;
+        }
+        
+        
+        for(int k = 1;k<n+1;k++){
+            for(int i=1;i<n+1;i++){
+                for(int j=1;j<n+1;j++){
+                    if(graph[i][k] && graph[k][j]){
+                        graph[i][j]= true;
+                    }
+                }
+            }
+        }
+        
+        for(int i =1;i<n+1;i++){
+            int cnt=0;
+            for(int j = 1;j<n+1;j++){
+                if(graph[i][j] || graph[j][i]) cnt++;
+            }
+            
+            if(cnt==n-1){
+                answer++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
